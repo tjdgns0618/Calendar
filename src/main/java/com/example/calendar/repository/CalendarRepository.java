b880bd04be2518107567d47717dd9de9ab8fd03d
tjdgns0618 <gruddufs@gmail.com>
2026-04-09 16:30:19 +0900

    fix : 작성자이름에 해당하는 모든 일정 조회, id에 해당하는 일정 단 건 조회

--- src/main/java/com/example/calendar/repository/CalendarRepository.java
+++ src/main/java/com/example/calendar/repository/CalendarRepository.java
@@ -3,5 +3,9 @@ package com.example.calendar.repository;
 import com.example.calendar.entity.Calendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+    // 이름에 해당하는 모든 데이터 찾기용
+    List<Calendar> findAllByWriterName(String writerName);
 }
