162d31b2b1df97b1fed079a0925f93568deaa2a6
tjdgns0618 <gruddufs@gmail.com>
2026-04-09 12:07:51 +0900

    feat : 캘린더 엔티티 추가와 JpaAuditing 적용을 위한 BaseEntity 추가 및 상속

--- src/main/java/com/example/calendar/CalendarApplication.java
+++ src/main/java/com/example/calendar/CalendarApplication.java
@@ -1,9 +1,12 @@
 package com.example.calendar;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @SpringBootApplication
+@EntityListeners(AuditingEntityListener.class)
 public class CalendarApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/calendar/entity/Calendar.java
@@ -0,0 +1,28 @@
+package com.example.calendar.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "calendars")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Calendar extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String calendarName;
+    private String calendarContents;
+    private String writerName;
+    private String password;
+
+    public Calendar(String calendarName, String calendarContents, String writerName, String password) {
+        this.calendarName = calendarName;
+        this.calendarContents = calendarContents;
+        this.writerName = writerName;
+        this.password = password;
+    }
+}
