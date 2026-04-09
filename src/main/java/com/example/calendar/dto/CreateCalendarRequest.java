d3b5f990f38c0fc0d1f5329762d09d3cb51b67d4
tjdgns0618 <gruddufs@gmail.com>
2026-04-09 14:04:52 +0900

    fix : 일정 생성 요청 DTO에 필요없는 필드 생성일, 수정일을 제거

--- src/main/java/com/example/calendar/dto/CreateCalendarRequest.java
+++ src/main/java/com/example/calendar/dto/CreateCalendarRequest.java
@@ -2,14 +2,10 @@ package com.example.calendar.dto;
 
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class CreateCalendarRequest {
     private String calendarName;
     private String calendarContents;
     private String writerName;
     private String password;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
 }
